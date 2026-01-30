0f33eccaa3045e1eda73f9f47cd25d6a165743ac
devkkd <developmentkontentkraftdigital.com>
2026-01-30 17:58:24 +0530

    add exhibitions

--- app/admin/layout/Sidebar.jsx
+++ app/admin/layout/Sidebar.jsx
@@ -29,6 +29,7 @@ const navigation = [
   { name: 'Subcategory', href: '/admin/subcategories', icon: CreditCardIcon },
   { name: 'Bulk Upload', href: '/admin/upload-bulk-products', icon: BellIcon },
   { name: 'Custom Orders', href: '/admin/custom-orders', icon: CogIcon },
+   { name: 'Exhibitions', href: '/admin/exhibitions', icon: CogIcon },
 ];
 
 
